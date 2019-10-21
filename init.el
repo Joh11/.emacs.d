28f0d3d08286582909cf0dbf9ee2a4791c70315a
Joh11 <johan74470@gmail.com>
2019-10-21 15:27:05 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(package-initialize)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Documents/EPFL/org/logement.org" "~/Documents/EPFL/org/todo.org" "~/Documents/EPFL/org/agenda.org")))
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update org-download org-bullets elfeed elfeed-org slime counsel ivy jedi which-key try rainbow-delimiters use-package)))
+ '(python-shell-interpreter "python3" t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'narrow-to-region 'disabled nil)
