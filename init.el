b3c00f955085f9963079924ca45af0e5fa4228fb
Johan Félisaz <johan.felisaz@protonmail.com>
2020-05-10 07:49:19 +0200

    added deft

--- init.el
+++ init.el
@@ -7,12 +7,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Documents/EPFL/org/logement.org" "~/Documents/EPFL/org/todo.org" "~/Documents/EPFL/org/agenda.org")))
+ '(org-agenda-files (quote ("~/Documents/dump.org")))
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update org-download org-bullets elfeed elfeed-org slime counsel ivy jedi which-key try rainbow-delimiters use-package)))
+    (deft gnu-elpa-keyring-update org-download org-bullets elfeed elfeed-org slime counsel ivy jedi which-key try rainbow-delimiters use-package)))
  '(python-shell-interpreter "python3" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
