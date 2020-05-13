9601e7f6772272df0034a30cb3d3f85b5fc846b3
Johan Félisaz <johan.felisaz@protonmail.com>
2020-05-13 12:36:53 +0200

    update init.el org agenda files

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Documents/dump.org")))
+ '(org-agenda-files (quote ("~/Documents/org/dump.org")))
  '(package-selected-packages
    (quote
     (deft gnu-elpa-keyring-update org-download org-bullets elfeed elfeed-org slime counsel ivy jedi which-key try rainbow-delimiters use-package)))
