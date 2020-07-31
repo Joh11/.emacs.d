c85bbe1daa0bc4f58fcb00be16e1616a7b2ecd1a
Johan Félisaz <johan.felisaz@protonmail.com>
2020-07-31 11:30:44 +0200

    change theme and change to imagemagick

--- init.el
+++ init.el
@@ -7,6 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0cb1b0ea66b145ad9b9e34c850ea8e842c4c4c83abe04e37455a1ef4cc5b8791" default)))
  '(org-agenda-files (quote ("~/Documents/org/dump.org")))
  '(org-archive-location "~/Documents/org/archive.org::* From %s")
  '(package-selected-packages
